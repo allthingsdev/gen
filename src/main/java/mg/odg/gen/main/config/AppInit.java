b50afac896328c101a17986bac47c989acd46a3d
allthingsdev <allthingsdev2017@gmail.com>
2018-12-12 21:44:24 +0300

    config changes

---
+++ src/main/java/mg/odg/gen/main/config/AppInit.java
@@ -0,0 +1,22 @@
+package mg.odg.gen.main.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {MvcConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
--- src/main/java/mg/odg/gen/main/config/MvcConfig.java
+++ src/main/java/mg/odg/gen/main/config/MvcConfig.java
@@ -5,20 +5,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
-public class WebConfig implements WebMvcConfigurer {
+public class MvcConfig {
 
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		
-		resolver.setPrefix("");
-		resolver.setSuffix("");
+		resolver.setPrefix("/");
+		resolver.setSuffix(".uhj");
 		resolver.setExposeContextBeansAsAttributes(true);
 		
 		return resolver;
