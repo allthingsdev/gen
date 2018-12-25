621fdb9936fac5a605cd8a20c28de985488bc871
allthingsdev <allthingsdev2017@gmail.com>
2018-12-25 09:16:41 +0300

    Update MvcConfig.java, login.html, and 2 more files...

--- src/main/java/mg/odg/gen/main/config/MvcConfig.java
+++ src/main/java/mg/odg/gen/main/config/MvcConfig.java
@@ -2,14 +2,9 @@ package mg.odg.gen.main.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 @ComponentScan
@@ -22,25 +17,8 @@ public class MvcConfig implements WebMvcConfigurer {
 		
 	}
 	
-	@Bean
-    @Description("Thymeleaf template resolver serving HTML 5")
-    public ClassLoaderTemplateResolver templateResolver() {
-        
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        
-        templateResolver.setPrefix("templates/");
-        templateResolver.setCacheable(false);
-        templateResolver.setSuffix(".html");        
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setCharacterEncoding("UTF-8");
-        
-        return templateResolver;
-    }
 	
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
-		registry.addResourceHandler("/resources/css/**").addResourceLocations("/resources/css/");
-	}
+	
 	
 }
 
